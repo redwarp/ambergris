e3dff198f71ce9180cdbb145976a5f9182a1bf65
Redwarp <redwarp@gmail.com>
2020-10-02 16:51:14 +0200

    Rltk is used for comparison

--- field_of_vision/benches/fov_bench.rs
+++ field_of_vision/benches/fov_bench.rs
@@ -2,6 +2,8 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use field_of_vision::FovMap;
 use field_of_vision::SampleMap;
 use rand::{rngs::StdRng, Rng, SeedableRng};
+use rltk::Algorithm2D;
+use tcod::Map;
 
 const WIDTH: isize = 45;
 const HEIGHT: isize = 45;
@@ -9,6 +11,34 @@ const POSITION_X: isize = 22;
 const POSITION_Y: isize = 22;
 const RADIUS: isize = 12;
 const RANDOM_WALLS: isize = 10;
+
+struct RltkMap {
+    width: isize,
+    height: isize,
+    transparents: Vec<bool>,
+}
+
+impl RltkMap {
+    fn set_transparent(&mut self, x: isize, y: isize, transparent: bool) {
+        self.transparents[(x + y * self.width) as usize] = transparent;
+    }
+
+    fn compute_fov(&mut self, x: isize, y: isize, radius: isize) {
+        rltk::field_of_view((x as i32, y as i32).into(), radius as i32, self);
+    }
+}
+
+impl rltk::BaseMap for RltkMap {
+    fn is_opaque(&self, index: usize) -> bool {
+        !self.transparents[index]
+    }
+}
+
+impl Algorithm2D for RltkMap {
+    fn dimensions(&self) -> rltk::Point {
+        (self.width as i32, self.height as i32).into()
+    }
+}
 
 pub fn fov_benchmark_no_walls(c: &mut Criterion) {
     let mut fov = FovMap::new(WIDTH, HEIGHT);
@@ -55,7 +85,7 @@ pub fn vec_fov_benchmark_random_walls(c: &mut Criterion) {
 }
 
 pub fn tcod_benchmark_no_walls(c: &mut Criterion) {
-    let mut map = tcod::Map::new(WIDTH as i32, HEIGHT as i32);
+    let mut map = Map::new(WIDTH as i32, HEIGHT as i32);
     for x in 0..WIDTH as i32 {
         for y in 0..HEIGHT as i32 {
             map.set(x, y, true, true);
@@ -71,7 +101,7 @@ pub fn tcod_benchmark_no_walls(c: &mut Criterion) {
 }
 
 pub fn tcod_benchmark_random_walls(c: &mut Criterion) {
-    let mut map = tcod::Map::new(WIDTH as i32, HEIGHT as i32);
+    let mut map = Map::new(WIDTH as i32, HEIGHT as i32);
     for x in 0..WIDTH as i32 {
         for y in 0..HEIGHT as i32 {
             map.set(x, y, true, true);
@@ -93,6 +123,37 @@ pub fn tcod_benchmark_random_walls(c: &mut Criterion) {
     });
 }
 
+pub fn rltk_benchmark_no_walls(c: &mut Criterion) {
+    let mut map = RltkMap {
+        width: WIDTH,
+        height: HEIGHT,
+        transparents: vec![true; (WIDTH * HEIGHT) as usize],
+    };
+
+    c.bench_function("rltk_benchmark_no_walls", |bencher| {
+        bencher.iter(|| map.compute_fov(POSITION_X, POSITION_Y, RADIUS));
+    });
+}
+
+pub fn rltk_benchmark_random_walls(c: &mut Criterion) {
+    let mut map = RltkMap {
+        width: WIDTH,
+        height: HEIGHT,
+        transparents: vec![true; (WIDTH * HEIGHT) as usize],
+    };
+
+    let mut rng = StdRng::seed_from_u64(42);
+    for _ in 0..RANDOM_WALLS {
+        let (x, y) = (rng.gen_range(0, WIDTH), rng.gen_range(0, HEIGHT));
+        map.set_transparent(x, y, false);
+    }
+    map.set_transparent(POSITION_X, POSITION_Y, true);
+
+    c.bench_function("rltk_benchmark_random_walls", |bencher| {
+        bencher.iter(|| map.compute_fov(POSITION_X, POSITION_Y, RADIUS));
+    });
+}
+
 criterion_group!(
     benches,
     fov_benchmark_no_walls,
@@ -100,6 +161,8 @@ criterion_group!(
     vec_fov_benchmark_no_walls,
     vec_fov_benchmark_random_walls,
     tcod_benchmark_no_walls,
-    tcod_benchmark_random_walls
+    tcod_benchmark_random_walls,
+    rltk_benchmark_no_walls,
+    rltk_benchmark_random_walls
 );
 criterion_main!(benches);
