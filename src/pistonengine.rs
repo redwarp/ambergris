570ce1adf4eb080f402aa030ad77b2c493b3145e
Redwarp <redwarp@gmail.com>
2020-10-03 16:45:36 +0200

    We have some text on screen!

--- src/pistonengine.rs
+++ src/pistonengine.rs
@@ -1,17 +1,16 @@
-use crate::components::*;
+use graphics::character::CharacterCache;
 use legion::*;
 use piston_window::types::Color as PistonColor;
 use piston_window::Graphics;
+use piston_window::PistonWindow;
 use piston_window::*;
-use piston_window::{PistonWindow, Window};
 
+use crate::game::{RunState, State};
+use crate::resources::*;
+use crate::systems;
 use crate::{
     components::{Body, Player},
     map::Map,
-};
-use crate::{
-    game::{RunState, State},
-    resources::PlayerInfo,
 };
 use field_of_vision::FovMap;
 
@@ -65,36 +64,82 @@ impl Engine {
     }
 
     pub fn run(&mut self, state: &mut State) {
-        let mut events = Events::new(EventSettings::new().lazy(true));
+        let mut events = Events::new(EventSettings::new().lazy(true).ups(2));
         let mut window = self.window.take().unwrap();
 
+        let texture_settings = TextureSettings::new().filter(Filter::Nearest);
+        let texture_context = window.create_texture_context();
+        let mut glyphs = Glyphs::new(
+            "CourierPrime-Regular.ttf",
+            texture_context,
+            texture_settings,
+        )
+        .expect("Couldn't load the font.");
+
+        let mut schedule = systems::game_schedule();
+
+        let mut previous_position = state.resources.get::<PlayerInfo>().unwrap().position;
+
         while let Some(event) = events.next(&mut window) {
-            window.draw_2d(&event, |context, graphics, _device| {
-                self.render_all(graphics, context, state);
-            });
+            let previous_state = state.resources.get_or_insert(RunState::Init).clone();
+            let new_run_state = match previous_state {
+                RunState::Init => {
+                    schedule.execute(&mut state.world, &mut state.resources);
+                    RunState::WaitForInput
+                }
+                RunState::PlayerTurn => {
+                    schedule.execute(&mut state.world, &mut state.resources);
+                    RunState::AiTurn
+                }
+                RunState::AiTurn => {
+                    schedule.execute(&mut state.world, &mut state.resources);
+                    RunState::WaitForInput
+                }
+                RunState::WaitForInput => RunState::WaitForInput,
+                RunState::Exit => break,
+            };
+
+            if let Some(_args) = event.render_args() {
+                let updated_position = state.resources.get::<PlayerInfo>().unwrap().position;
+
+                self.prepare_console(state, previous_position != updated_position);
+                window.draw_2d(&event, |context, graphics, _device| {
+                    clear(BLACK.into(), graphics);
+                    self.console.render(
+                        graphics,
+                        context,
+                        &mut glyphs,
+                        (0, 0),
+                        (self.console.width, self.console.height),
+                        (0, 0),
+                    );
+                });
+
+                previous_position = updated_position;
+            }
+
+            state.resources.insert(new_run_state);
         }
 
         self.window = Some(window);
     }
 
-    pub fn render_all<G: Graphics>(
-        &mut self,
-        graphics: &mut G,
-        context: Context,
-        state: &mut State,
-    ) {
-        clear([0.0, 0.0, 0.0, 1.0], graphics);
+    pub fn prepare_console(&mut self, state: &mut State, compute_fov: bool) {
         self.console.clear();
-        self.render_map(graphics, context, state, true);
+        self.prepare_map(state, compute_fov);
+
+        let fov = state.resources.get::<FovMap>().unwrap();
+
+        let mut query = <&Body>::query();
+        for body in query.iter(&state.world) {
+            if fov.is_in_fov(body.x as isize, body.y as isize) {
+                self.console
+                    .set_foreground(body.x, body.y, body.char, body.color);
+            }
+        }
     }
 
-    fn render_map<G: Graphics>(
-        &mut self,
-        graphics: &mut G,
-        context: Context,
-        state: &mut State,
-        fov_recompute: bool,
-    ) {
+    fn prepare_map(&mut self, state: &mut State, fov_recompute: bool) {
         let mut map = state.resources.get_mut::<Map>().unwrap();
         let mut fov = state.resources.get_mut::<FovMap>().unwrap();
 
@@ -133,9 +178,6 @@ impl Engine {
                 }
             }
         }
-
-        self.console
-            .render(graphics, context, (0, 0), (map_width, map_height), (0, 0));
     }
 }
 
@@ -180,10 +222,22 @@ impl Into<PistonColor> for Color {
     }
 }
 
+impl Into<Color> for tcod::Color {
+    fn into(self) -> Color {
+        Color {
+            a: 255,
+            r: self.r,
+            g: self.g,
+            b: self.b,
+        }
+    }
+}
+
 struct Console {
     width: i32,
     height: i32,
-    background: Vec<Color>,
+    background: Vec<Option<Color>>,
+    foreground: Vec<Option<(char, Color)>>,
 }
 
 impl Console {
@@ -191,13 +245,14 @@ impl Console {
         Console {
             width,
             height,
-            background: vec![BLACK; (width * height) as usize],
+            background: vec![None; (width * height) as usize],
+            foreground: vec![None; (width * height) as usize],
         }
     }
 
     fn clear(&mut self) {
         for color in self.background.iter_mut() {
-            *color = BLACK;
+            *color = None;
         }
     }
 
@@ -210,13 +265,18 @@ impl Console {
     }
 
     fn set_background(&mut self, x: i32, y: i32, color: Color) {
-        self.background[(x + y * self.width) as usize] = color
+        self.background[(x + y * self.width) as usize] = Some(color);
     }
 
-    fn render<G: Graphics>(
+    fn set_foreground<C: Into<Color>>(&mut self, x: i32, y: i32, glyph: char, color: C) {
+        self.foreground[(x + y * self.width) as usize] = Some((glyph, color.into()));
+    }
+
+    fn render(
         &self,
-        graphics: &mut G,
+        graphics: &mut G2d,
         context: Context,
+        glyphs: &mut Glyphs,
         (origin_x, origin_y): (i32, i32),
         (origin_width, origin_height): (i32, i32),
         (destination_x, destination_y): (i32, i32),
@@ -226,13 +286,32 @@ impl Console {
 
         for x in origin_x..origin_width {
             for y in origin_y..origin_height {
-                let color: PistonColor = self.background[(x + y * self.width) as usize].into();
-                let square = graphics::rectangle::square(
+                let (draw_x, draw_y) = (
                     ((x + dx) * GRID_SIZE as i32) as f64,
                     ((y + dy) * GRID_SIZE as i32) as f64,
-                    GRID_SIZE as f64,
                 );
-                graphics::rectangle(color, square, context.transform, graphics);
+                if let Some(color) = self.background[(x + y * self.width) as usize] {
+                    let color: PistonColor = color.into();
+
+                    let square = graphics::rectangle::square(
+                        ((x + dx) * GRID_SIZE as i32) as f64,
+                        ((y + dy) * GRID_SIZE as i32) as f64,
+                        GRID_SIZE as f64,
+                    );
+                    graphics::rectangle(color, square, context.transform, graphics);
+                }
+
+                if let Some((glyph, color)) = self.foreground[(x + y * self.width) as usize] {
+                    text::Text::new_color(color.into(), GRID_SIZE)
+                        .draw(
+                            &format!("{}", glyph),
+                            glyphs,
+                            &context.draw_state,
+                            context.transform.trans(draw_x, draw_y),
+                            graphics,
+                        )
+                        .expect("Could not draw glyph");
+                }
             }
         }
     }
