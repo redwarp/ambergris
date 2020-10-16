28f654c0d601d7ce3b7e28f2918b99902ecbfacf
Benoit Vermont <redwarp@gmail.com>
2020-10-16 19:13:55 +0200

    Update deps and change overlay color

--- src/palette.rs
+++ src/palette.rs
@@ -2,5 +2,5 @@ use crate::colors::Color;
 
 pub const WINDOW_BACKGROUND: Color = Color::from_argb(0xaa000000);
 pub const SELECTED: Color = Color::from_argb(0x66ff0000);
-pub const OVERLAY: Color = Color::from_argb(0x6600ff00);
+pub const OVERLAY: Color = Color::from_argb(0x3300ff00);
 pub const HEALTH: Color = Color::new(255, 191, 0, 0);
